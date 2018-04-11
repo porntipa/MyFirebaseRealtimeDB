3903b8b4b8bf64f0ec69cd6d688b54f9335756da
Porntipa Choksungnoen <porntipa.chok@gmail.com>
2018-04-11 11:24:46 +0700

    display font ค่า 0

--- app/src/main/java/th/or/nectec/myfirebaserealtimedb/MainActivity.java
+++ app/src/main/java/th/or/nectec/myfirebaserealtimedb/MainActivity.java
@@ -1,13 +1,26 @@
 package th.or.nectec.myfirebaserealtimedb;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
+
+    public DatabaseReference myRef;
+    private TextView mFirebaseTextView;
+    private int mCounter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseTextView = (TextView) findViewById(R.id.firebaseTextView);
+        Typeface type = Typeface.createFromAsset(getAssets(), "ds_digital.TTF");
+        mFirebaseTextView.setTypeface(type);
     }
 }
